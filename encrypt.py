23f56b4a3f3b6b83f793848d37cf19fe18425e6d
AravDeGit <dearav3@gmail.com>
2024-12-06 08:38:55 +0000

    Encryption complete

--- encrypt.py
+++ encrypt.py
@@ -1,21 +1,43 @@
-import random
-class Enigma:
-    def key():
-        keyList = []
-        rand = random.randint(10000, 99999) # General soltuion : argument: n for the number of digits, use n as a power of 10 for lower and upper limit. 
-        free = rand
-        while free > 10:
-            div = 100 # General solution: use n-1 power of 10 and keep dividing by 10.
-            d = int(free % div)
-            free = int(free / 10)
-            keyList.append(d)
-            d = 0
-            div = int(div/10)
-        print(rand)
-        print(keyList.reverse())
-        return keyList.reverse()
+import string
 
-    # def offset():
-    # def encrypt():
-    if __name__ == "__main__": 
-        print(key())
+class EnigmaCipher:
+    def __init__(self, key, date):
+        self.char_set = list(string.ascii_lowercase) + [" "]
+        self.char_map = {char: i for i, char in enumerate(self.char_set)}
+        self.key = key
+        self.date = date
+        self.offsets = self.calculate_offsets()
+
+    def calculate_offsets(self):
+        date_squared = int(self.date) ** 2
+        offset_digits = str(date_squared)[-len(self.char_set):]
+        return [int(d) for d in offset_digits]
+
+    def encrypt(self, plaintext):
+        plaintext = plaintext.lower()
+        encrypted_message = []
+
+        for i, char in enumerate(plaintext):
+            if char not in self.char_map:
+                encrypted_message.append(char)
+                continue
+
+            shift_idx = i % len(self.offsets)
+            total_shift = (self.key + self.offsets[shift_idx]) % len(self.char_set)
+            char_pos = self.char_map[char]
+            enc_pos = (char_pos + total_shift) % len(self.char_set)
+            encrypted_message.append(self.char_set[enc_pos])
+
+        return "".join(encrypted_message)
+
+def main():
+    date = "12032024"
+    key = 7
+    plaintext = "hello world"
+
+    enigma = EnigmaCipher(key=key, date=date)
+    encrypted_message = enigma.encrypt(plaintext)
+    print(f"Encrypted Message: {encrypted_message}")
+
+if __name__ == "__main__":
+    main()
